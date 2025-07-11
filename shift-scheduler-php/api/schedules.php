df5496b35ae41f3b4149d3740017bf1a76790ec3
SCP-015 <92101977+SCP-015@users.noreply.github.com>
2025-07-11 15:14:00 +0700

    Update schedules.php

--- shift-scheduler-php/api/schedules.php
+++ shift-scheduler-php/api/schedules.php
@@ -15,7 +15,7 @@ $users = [
     "004" => "Yohan"
 ];
 
-$patternStart = "2024-12-26";
+$patternStart = "2024-12-23";
 
 $startDate = $_GET['start_date'] ?? null;
 $endDate = $_GET['end_date'] ?? null;
